984de5c5492e0e167bb8fad90de653f616883226
An Le <anle@chotot.vn>
2021-08-22 17:43:38 +0700

    chore: data

--- run-manager.sh
+++ run-manager.sh
@@ -1,1 +1,5 @@
 rabbitmq-plugins enable rabbitmq_management
+
+mongorestore -h localhost:27017  -d reservation --drop --batchSize=100 ./reservation
+
+mongorestore -h localhost:27017  -d user-profiler --drop --batchSize=100 ./user-profiler
