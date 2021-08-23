23b1a9190f56cfff553b19e569d3fa2d1142fc73
An Le <anle@chotot.vn>
2021-08-23 23:45:32 +0700

    fix: update transactions

--- README.md
+++ README.md
@@ -52,4 +52,4 @@ https://www.getpostman.com/collections/ff7b9533a8931b1dba3f
 <img src="https://i.ibb.co/N1n07V9/Vaccine-reservation.jpg" height="600" width="800" hspace="40">
 
 ### Sequence Diagram
-<img src="https://i.ibb.co/Rp1CMtN/Untitled.png" height="1500" width="800" hspace="40">
+<img src="https://i.ibb.co/9HNwr2b/Untitled.png" height="1600" width="800" hspace="40">
--- backend/reservation/src/workers/reservation.js
+++ backend/reservation/src/workers/reservation.js
@@ -24,70 +24,7 @@ AMQP.get_connection(configs.RABBIT_CONFIG).then((connection) => {
       logger.info('data Queue:', data);
       console.log('data', data)
 
-      const customer = await createCustomer(data);
-
-      const reservation = await Reservation.findOne({ customerID: new Types.ObjectId(customer._id) })
-      if (reservation) {
-        logger.error('Error: this customer registered');
-        console.log('Error: this customer registered')
-        ack.reject(false);
-        return
-      }
-
-      let nurseAssign = null
-      let branchAssign = null
-      let serveDay = new Date()
-
-      let session = null;
-      for (let i = 0; i < MAX_DAYS_EXTEND; i ++) {
-        const sessionTemp = await db.startSession();
-        const startDate = new Date();
-        startDate.setDate(startDate.getDate() + i);
-        startDate.setUTCHours(0,0,0,0);
-  
-        const endDate = new Date();
-        endDate.setDate(endDate.getDate() + i);
-        endDate.setUTCHours(23,59,59,999);
-
-        const branches = await findBranches(startDate, endDate, sessionTemp)
-
-        let isFindAssign = false
-
-        for (let j = 0; j < branches.length; j ++) {
-          const branch = branches[j]
-          if (branch.capacity <= branch.count) continue
-          isFindAssign = true
-          branchAssign = branch
-          serveDay = startDate
-          nurseAssign = await findNurse(branch)
-          break
-        }
-  
-        if (!isFindAssign) {
-          sessionTemp.endSession()
-          continue
-        } else {
-          session = sessionTemp
-          break
-        }
-      }
-
-      const reservationData = {
-        branch: branchAssign._id,
-        centre: branchAssign.centre,
-        nurseID: nurseAssign,
-        customerID: customer._id,
-        serveDay,
-      }
-
-      const newReservation = new Reservation(reservationData)
-      await newReservation.save()
-      if (session) session.endSession()
-
-      // send notification or sms or websocket to nurse for is coming reservation
-      // send notification or sms for user for get success reservation
-      ///
-
+      await processReservation(data, ack)
       ack.acknowledge();
     } catch (ex) {
       logger.error('Error:', ex.message);
@@ -97,7 +34,86 @@ AMQP.get_connection(configs.RABBIT_CONFIG).then((connection) => {
   });
 });
 
-const findBranches = async (startDate, endDate, session) => {
+const processReservation = async (data, ack) => {
+  const customer = await createCustomer(data);
+
+  const reservation = await Reservation.findOne({ customerID: new Types.ObjectId(customer._id) })
+  if (reservation) {
+    logger.error('Error: this customer registered');
+    console.log('Error: this customer registered')
+    ack.reject(false);
+    return
+  }
+
+  let nurseAssign, branchAssign, startDateTemp, endDateTemp = null
+  let serveDay = new Date()
+
+  for (let i = 0; i < MAX_DAYS_EXTEND; i ++) {
+    const startDate = new Date();
+    startDate.setDate(startDate.getDate() + i);
+    startDate.setUTCHours(0,0,0,0);
+    startDateTemp = startDate
+    
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() + i);
+    endDate.setUTCHours(23,59,59,999);
+    endDateTemp = endDate
+
+    const branches = await findBranches(startDate, endDate)
+
+    let isFindAssign = false
+
+    for (let j = 0; j < branches.length; j ++) {
+      const branch = branches[j]
+      if (branch.capacity <= branch.count) continue
+      isFindAssign = true
+      branchAssign = branch
+      serveDay = startDate
+      nurseAssign = await findNurse(branch)
+      break
+    }
+
+    if (!isFindAssign) {
+      continue
+    } else {
+      break
+    }
+  }
+
+  const reservationData = {
+    branch: branchAssign._id,
+    centre: branchAssign.centre,
+    nurseID: nurseAssign,
+    customerID: customer._id,
+    serveDay,
+  }
+
+  const session = await db.startSession();
+  const newReservation = new Reservation(reservationData)
+  await session.withTransaction(() => {
+    return newReservation.save()
+  });
+  const branchTemp = await findBranch(branchAssign._id, startDateTemp, endDateTemp)
+  console.log('branchTemp', branchTemp)
+  let isAbortTransaction = false
+  if (branchTemp.count <= branchTemp.capacity) {
+    console.log('end session')
+    session.endSession()
+  } else {
+    console.log('abort session')
+    isAbortTransaction = true
+    session.abortTransaction()
+    await processReservation(data, ack)
+  }
+  
+  if (!isAbortTransaction) {
+    // send notification or sms or websocket to nurse for is coming reservation
+    // send notification or sms for user for get success reservation
+    ///
+  }
+}
+
+const findBranches = async (startDate, endDate) => {
   const objQueryMatch = {
     $match: {
       disabled: false,
@@ -148,8 +164,62 @@ const findBranches = async (startDate, endDate, session) => {
     // {
     //   $limit: 1,
     // },
-  ]).session(session)
+  ])
   return branches
+}
+
+const findBranch = async (_id, startDate, endDate) => {
+  const objQueryMatch = {
+    $match: {
+      disabled: false,
+      _id: new Types.ObjectId(_id)
+    },
+  }
+
+  const objQueryProject = {
+    $project: {
+      count: {
+        $size: '$reservations',
+      },
+      capacity: '$capacity',
+      centre: '$centre',
+    },
+  }
+  const objQueryLookup = {
+    $lookup: {
+      from: 'reservations',
+      let: {
+        branchID: '$_id',
+      },
+      pipeline: [
+        {
+          $match: {
+            $expr: {
+              $and: [
+                { $eq: ['$branch', '$$branchID'] },
+                { $gte: ['$serveDay', new Date(startDate)] },
+                { $lte: ['$serveDay', new Date(endDate)] },
+              ],
+            },
+          },
+        },
+      ],
+      as: 'reservations',
+    },
+  }
+
+  const branches = await Branch.aggregate([
+    objQueryMatch,
+    objQueryLookup,
+    objQueryProject,
+    {
+      $sort: {
+        count: 1,
+      },
+    },
+  ])
+
+  return branches[0] || {}
 }
 
 const findNurse = async (branch) => {
